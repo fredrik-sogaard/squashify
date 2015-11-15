6edebb8dd133156b2b3f009f1630e9f6bc0c08c1
evenv <even@vinge.cc>
2015-11-15 16:26:11 +0100

    Litt mer testin

--- app/controllers/bot_controller.rb
+++ app/controllers/bot_controller.rb
@@ -1,9 +1,10 @@
 class BotController < ApplicationController
 
 def index
+
   render json: {
     response_type: "in_channel",
-    text: "Hei!",
+    text: "Hei " + params[:user_name],
     attachments: [ { text:"Hallo hallo" } ]
   }
 end
