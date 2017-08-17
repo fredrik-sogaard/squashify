2001743d9583d8fdcbaf346df83ed989c745da10
Fredrik Søgaard <fredrik-sogaard@users.noreply.github.com>
2017-08-17 13:31:56 +0200

    Show more days

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,7 +5,7 @@ class BookingsController < ApplicationController
     @date = params[:id].nil? ? Date.today+1 : Date.today + params[:id].to_i
 
     days = t(:"date.abbr_day_names")
-    @dates = (0..2).map do |id|
+    @dates = (0..4).map do |id|
       day = Date.today + id
 
       [ id , days[day.wday], day==@date ]
